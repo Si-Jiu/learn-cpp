53141b70597f69653f3e0b16d1687fb184f0f2ac
Si-Jiu49 <si_jiu49@outlook.com>
2023-01-28 16:52:17 +0800

    first commit

---
+++ src/tests/test.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file test.cpp
+ * @author 肆鸠 (si_jiu49@outlook.com)
+ * @brief
+ * @date 2023-01-27
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short aaa;
+
+    return 0;
+}
