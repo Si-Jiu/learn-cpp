8f46e2b7e2c7d3cb980d9515f5310cf51198fc43
Si-Jiu49 <si_jiu49@outlook.com>
2023-02-05 12:32:28 +0800

    日常提交 2023/2/5

---
+++ src/Luogu/Problem/P5703.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file P5703.cpp
+ * @author 肆鸠 (si_jiu49@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-02-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
