d813c5d57036791de1b452de71ea73bc84b0b01a
Si-Jiu49 <si_jiu49@outlook.com>
2023-02-17 23:23:50 +0800

    准备不用WSL了，速度慢

--- src/Luogu/Problem/P2010.cpp
+++ src/Luogu/Problem/P2010.cpp
@@ -19,6 +19,26 @@ struct date
     short year, month, day;
 };
 
+bool IsPalindrome(string o)
+{
+    int l = 0, r = o.length() - 1;
+    while (true)
+    {
+        if (l == r || l == r - 1)
+        {
+            break;
+        }
+        if (o[l] == o[r])
+        {
+            l++;
+            r--;
+            continue;
+        }
+        return false;
+    }
+    return true;
+}
+
 int main()
 {
     /*
@@ -28,12 +48,17 @@ int main()
     t1 用字符串的方法输入的一个临时变量
     t2 同上
 
-*/
-    date date1, date2;
+    */
     string t1, t2;
-
-    // 输入
     cin >> t1 >> t2;
+
+    if (t1 == t2)
+    {
+        cout << IsPalindrome(t1) << endl;
+    }
+
+    date date1, date2;
+    int count = 0;
 
     // 把 输入进去的字符串 切片 然后 转整数
     date1.year = stoi(t1.substr(0, 4));
@@ -50,9 +75,27 @@ int main()
         {
             for (short day = date1.day; day <= date2.day; day++)
             {
+                t1 = "";
+                t1 += to_string(year);
+                if (month < 10)
+                {
+                    t1 += "0";
+                }
+                t1 += to_string(month);
+                if (day < 10)
+                {
+                    t1 += "0";
+                }
+                t1 += to_string(day);
+                if (IsPalindrome(t1))
+                {
+                    count++;
+                }
             }
         }
     }
+
+    cout << count;
 
     return 0;
 }
